6c5d72e7981b195dfed75b43c28719e18420ecd7
Kingjux <154667110+Kingjux@users.noreply.github.com>
2024-03-08 02:30:43 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+// GitHub Configurations
+const config = {
+    github: {
+        token: "https://github.com/Kingjux",
+        repository: "https://github.com/Kingjux/Venocyber-voice-message",
+        branch: "main"
+    }
+};
+
+export default config;
