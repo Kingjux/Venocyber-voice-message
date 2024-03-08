8f86d7f2ef7c7f72109899225e2bb30487bce284
Kingjux <154667110+Kingjux@users.noreply.github.com>
2024-03-08 03:20:48 +0300

    Create jquery.js

---
+++ VENOCYBER/jquery.js
@@ -0,0 +1,23 @@
+{
+  "name": "venocyber-voice-message",
+  "version": "1.0.0",
+  "description": "Voice message web application",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "library1": "^1.0.0",
+    "library2": "^2.0.0"
+  },
+  "devDependencies": {
+    "library3": "^3.0.0",
+    "library4": "^4.0.0"
+  },
+  "repository": {
+    "type": "git",
+    "url": "git+https://github.com/kingjux/venocyber-voice-message.git"
+  },
+  "author": "Venocyber Tech",
+  "license": "MIT"
+}
